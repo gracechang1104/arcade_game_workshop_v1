af1d0fe6f8de44da3c920ce5818b33fc2f314b88
gracechang1104 <32424276+gracechang1104@users.noreply.github.com>
2019-09-27 23:55:49 -0700

    Update flower.py

--- flower.py
+++ flower.py
@@ -9,6 +9,7 @@ class Flower(arcade.Sprite):
     def setup(cls, flowers_list, bottom_obstacles_list, top_obstacles_list):
         number_of_flowers = random.randrange(1, 3)
         for i in range(number_of_flowers):
-            # make sure flowers are not being placed on top of obstacles
+            # TODO: make sure the flower is not being placed on top of obstacles, then append the flower to
+            # list of flowers
 
         return flowers_list
